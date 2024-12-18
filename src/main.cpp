6e59d5efad5f708d74c3d74fa994f245b1ab9417
alin <alin@rspsystems.com>
2024-12-18 14:08:02 +0100

    Change main to fail if SDL and SDL Image has not initialised

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,27 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
+    // Define the SDL flags for initialisation, which must be ready
+    // before able to start a game loop.
+    constexpr int SDL_FLAGS = SDL_INIT_VIDEO | SDL_INIT_TIMER;
 
-  // Create an application window with the following settings:
-  SDL_Window *window = SDL_CreateWindow(
-      "An SDL2 window",        //    const char* title
-      SDL_WINDOWPOS_UNDEFINED, //    int x: initial x position
-      SDL_WINDOWPOS_UNDEFINED, //    int y: initial y position
-      640,                     //    int w: width, in pixels
-      480,                     //    int h: height, in pixels
-      SDL_WINDOW_SHOWN         //    Uint32 flags: window options, see docs
-  );
+    // Define the SDL Image flags for initialisation.
+    constexpr int SDL_IMAGE_FLAGS = IMG_INIT_PNG;
 
-  // Check that the window was successfully made
-  if (window == nullptr) {
-    // In the event that the window could not be made...
-    std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-    SDL_Quit();
+    // Abort if SDL failed to initialise.
+    if (SDL_Init(SDL_FLAGS) != 0) {
+        std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    return 1;
-  }
+    // Abort if SDL Image failed to initialise
+    if (IMG_Init(SDL_IMAGE_FLAGS) == 0) {
+        std::cerr << "SDL Image Init Error: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-  // The window is open: enter program loop (see SDL_PollEvent)
-  SDL_Delay(3000); // Wait for 3000 milliseconds, before proceeding...
-
-  // Close and destroy the window
-  SDL_DestroyWindow(window);
-
-  // Clean up SDL2 and exit the program
-  SDL_Quit();
-
-  return 0;
+    return EXIT_SUCCESS;
 }
