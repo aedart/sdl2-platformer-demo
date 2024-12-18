de1b715f9b31b83b9015ed9ede042a83be2de423
alin <alin@rspsystems.com>
2024-12-18 15:38:58 +0100

    Init window and run game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+
 #include <iostream>
+
+#include "RenderWindow.h"
 
 int main(int argc, char *argv[])
 {
+    // -------------------------------------------------------------------------
+    // Initialisation
+    // -------------------------------------------------------------------------
+
     // Define the SDL flags for initialisation, which must be ready
     // before able to start a game loop.
     constexpr int SDL_FLAGS = SDL_INIT_VIDEO | SDL_INIT_TIMER;
@@ -22,6 +29,39 @@ int main(int argc, char *argv[])
         std::cerr << "SDL Image Init Error: " << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
     }
+
+    // Init the window
+    RenderWindow window("Game v.0.1.0", 1280, 720);
+
+    // -------------------------------------------------------------------------
+    // Game Loop
+    // -------------------------------------------------------------------------
+
+    // Game loop state
+    bool running = true;
+
+    // Event to listen for
+    SDL_Event event;
+
+    // While game is running...
+    while (running) {
+
+        // While polling for an SDL event...
+        while (SDL_PollEvent(&event)) {
+            // When user requests the window closed
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------------
+    // Destroy
+    // -------------------------------------------------------------------------
+
+    // Destroy the window and quit the SDL
+    window.destroy();
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
